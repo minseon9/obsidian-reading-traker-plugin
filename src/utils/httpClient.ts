893b0fb45ab731fb3e8beebbdb21ac1954e6f35b
minseon9 <dlaepeul@gmail.com>
2026-01-28 06:27:10 +0900

    refactor: remove unused HttpError class

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -58,18 +58,3 @@ export class HttpClient {
 		}
 	}
 }
-
-/**
- * HTTP error class
- */
-export class HttpError extends Error {
-	status: number;
-	statusText: string;
-
-	constructor(message: string, status: number, statusText: string) {
-		super(message);
-		this.name = 'HttpError';
-		this.status = status;
-		this.statusText = statusText;
-	}
-}
